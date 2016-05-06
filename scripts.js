583d445e8b73ffdaef417173e9588076b5d2dfa1
Eric Humpert <EricMac@Ricks-MacBook-Pro.local>
2016-05-06 15:40:48 -0500

    finished the functionality of the code for the assignment

--- scripts.js
+++ scripts.js
@@ -6,12 +6,13 @@ if (this.MorbidData === undefined) this.MorbidData = {};
   var apiData;
 
   function stateButtonBuilder(obj) {
-    for (var state of obj.data) {
+    console.log(obj);
+    obj.data.forEach(function (item, index) {
       var stateButton = document.createElement('li');
-      stateButton.setAttribute('data-state-id', state);
-      stateButton.textContent = state[8];
+      stateButton.setAttribute('data-state-id', index);
+      stateButton.textContent = item[8];
       statesUl.appendChild(stateButton);
-    }
+    })
   }
 
   function start() {
@@ -26,11 +27,31 @@ if (this.MorbidData === undefined) this.MorbidData = {};
       statesUl.addEventListener('click', clicked);
 
       function clicked(evt) {
-        console.log('clicky', evt.target.attributes['data-state-id'].value);
+
+        var stateNameSpan = document.querySelector('#state-name');
+        var ages0_20Span = document.querySelector('#ages-0-20');
+        var ages21_34Span = document.querySelector('#ages-21-34');
+        var ages35AndUpSpan = document.querySelector('#ages-35-and-up');
+        var maleDeathsSpan = document.querySelector('#male-deaths');
+        var femaleDeathsSpan = document.querySelector('#female-deaths');
 
         var index = evt.target.attributes['data-state-id'].value;
-        var stateIndex = apiData.data[index];
-        console.log('state', stateIndex);
+        var state = apiData.data[index];
+
+        var stateName = state[8];
+        var ages0_20_deaths = state[10];
+        var ages21_34_deaths = state[11];
+        var ages35_and_up_deaths = state[12];
+        var darwin_award_nominees = state[13];
+        var female_deaths = state[14];
+
+        stateNameSpan.textContent = stateName;
+        ages0_20Span.textContent = ages0_20_deaths;
+        ages21_34Span.textContent = ages21_34_deaths;
+        ages35AndUpSpan.textContent = ages35_and_up_deaths;
+        maleDeathsSpan.textContent = darwin_award_nominees;
+        femaleDeathsSpan.textContent = female_deaths;
+
       }
     })
     //Call your code here
