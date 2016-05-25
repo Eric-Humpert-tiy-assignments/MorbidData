441a1453e9450e6ce58d9482b58611c583ac72ff
Thwipshot <ethumpert@gmail.com>
2016-05-25 15:42:55 -0500

    made initial setup and code for es6 implementation

--- scripts.js
+++ scripts.js
@@ -2,48 +2,47 @@
 if (this.MorbidData === undefined) this.MorbidData = {};
 
 (function(context) {
-  var statesUl;
-  var apiData;
-  var statsHolder = document.querySelector('#stats-holder');
+  let statesUl;
+  let apiData;
+  let statsHolder = $('#stats-holder');
 
   function stateButtonBuilder(obj) {
-    var stateButtonList = '';
+    let stateButtonList = '';
     console.log(obj);
     obj.data.forEach(function (item, index) {
-      var stateButtonConstructor = _.template('<li data-state-id = "<%= index %>"> <%= stateName %> </li>');
-      var stateButton = stateButtonConstructor({'index': index,'stateName': item[8]});
+      let stateButtonConstructor = _.template('<li data-state-id = "<%= index %>"> <%= stateName %> </li>');
+      let stateButton = stateButtonConstructor({'index': index,'stateName': item[8]});
       stateButtonList += stateButton;
     })
-    statesUl.innerHTML = stateButtonList;
+      console.log(stateButtonList);
+    statesUl.html(stateButtonList);
+    console.log()
   }
 
   function clicked(evt) {
 
     if (evt.target !== statesUl) {
+      console.log('clicked a thing!');
+      let statsTemplate = $('#stats-template');
+      let index = evt.target.attributes['data-state-id'].value;
+      let state = apiData.data[index];
 
-      var statsTemplate = document.querySelector('#stats-template');
-      var index = evt.target.attributes['data-state-id'].value;
-      var state = apiData.data[index];
+      let statsListConstructor = _.template(statsTemplate.html());
 
-      var statsListConstructor = _.template(statsTemplate.innerHTML);
+      let statsList = statsListConstructor({'stateName': state[8], 'ages0_20_deaths': state[10], 'ages21_34_deaths': state[11], 'ages35_and_up_deaths': state[12], 'darwin_award_nominees': state[13], 'female_deaths': state[14]});
 
-      var statsList = statsListConstructor({'stateName': state[8], 'ages0_20_deaths': state[10], 'ages21_34_deaths': state[11], 'ages35_and_up_deaths': state[12], 'darwin_award_nominees': state[13], 'female_deaths': state[14]});
-
-      statsHolder.innerHTML = statsList;
+      statsHolder.html(statsList);
     }
   }
 
   function start() {
 
-    statesUl = document.querySelector('.state-buttons');
+    statesUl = $('.state-buttons');
 
-    $.ajax('https://data.cdc.gov/api/views/k9ai-xgx2/rows.json?accessType=DOWNLOAD').done(function(result) {
-      console.log('got the obj!', result);
-      apiData = result;
-      stateButtonBuilder(apiData);
-
-      statesUl.addEventListener('click', clicked);
-
+    $.ajax('https://data.cdc.gov/api/views/k9ai-xgx2/rows.json?accessType=DOWNLOAD').done(data => {
+      stateButtonBuilder(data);
+      apiData = data;
+      statesUl.click('click', clicked);
     })
     //Call your code here
     console.log('starting!', context);
